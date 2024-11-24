45b360c7c82d2325ab069b8882fdc1c6f7a74216
MiikaA1i <764704675@nzse.ac.nz>
2024-11-25 11:34:19 +1300

    testingBtn

--- SWD606_Assignment2/employeeDashboard.Designer.cs
+++ SWD606_Assignment2/employeeDashboard.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "employeeDashboard";
+            Testing = new CheckBox();
+            SuspendLayout();
+            // 
+            // Testing
+            // 
+            Testing.AutoSize = true;
+            Testing.Location = new Point(348, 195);
+            Testing.Name = "Testing";
+            Testing.Size = new Size(101, 24);
+            Testing.TabIndex = 0;
+            Testing.Text = "checkBox1";
+            Testing.UseVisualStyleBackColor = true;
+            // 
+            // employeeDashboard
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Testing);
+            Name = "employeeDashboard";
+            Text = "employeeDashboard";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private CheckBox Testing;
     }
 }
