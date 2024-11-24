a575f760b9532f40709e45a5e8f0ace84538748b
justi <justi@ASDFGHJKL123>
2024-11-25 12:19:50 +1300

    deleted styff

--- SWD606_Assignment2/employeeDashboard.Designer.cs
+++ SWD606_Assignment2/employeeDashboard.Designer.cs
@@ -28,33 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Testing = new CheckBox();
             SuspendLayout();
-            // 
-            // Testing
-            // 
-            Testing.AutoSize = true;
-            Testing.Location = new Point(348, 195);
-            Testing.Name = "Testing";
-            Testing.Size = new Size(101, 24);
-            Testing.TabIndex = 0;
-            Testing.Text = "checkBox1";
-            Testing.UseVisualStyleBackColor = true;
             // 
             // employeeDashboard
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(Testing);
             Name = "employeeDashboard";
             Text = "employeeDashboard";
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private CheckBox Testing;
     }
 }
