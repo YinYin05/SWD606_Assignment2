69b4ced91036e920549a6bb92072adc05e645fd9
justi <justi@ASDFGHJKL123>
2024-11-26 10:17:27 +1300

    Employee Dashboard update

--- SWD606_Assignment2/employeeDashboard.Designer.cs
+++ SWD606_Assignment2/employeeDashboard.Designer.cs
@@ -29,99 +29,122 @@
         private void InitializeComponent()
         {
             labelFirstName = new Label();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
             labelId = new Label();
             labelEmail = new Label();
             labelRole = new Label();
+            labelLeaveStatus = new Label();
+            buttonComplain = new Button();
+            labelAddress = new Label();
+            labelLastName = new Label();
+            labelPhone = new Label();
+            labelHourlyRate = new Label();
             SuspendLayout();
             // 
             // labelFirstName
             // 
             labelFirstName.AutoSize = true;
-            labelFirstName.Location = new Point(563, 159);
+            labelFirstName.Location = new Point(563, 91);
             labelFirstName.Name = "labelFirstName";
-            labelFirstName.Size = new Size(50, 20);
+            labelFirstName.Size = new Size(80, 20);
             labelFirstName.TabIndex = 0;
-            labelFirstName.Text = "label1";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(477, 159);
-            label1.Name = "label1";
-            label1.Size = new Size(80, 20);
-            label1.TabIndex = 1;
-            label1.Text = "First name:";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(477, 199);
-            label2.Name = "label2";
-            label2.Size = new Size(49, 20);
-            label2.TabIndex = 2;
-            label2.Text = "Email:";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(477, 233);
-            label3.Name = "label3";
-            label3.Size = new Size(42, 20);
-            label3.TabIndex = 3;
-            label3.Text = "Role:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(477, 121);
-            label4.Name = "label4";
-            label4.Size = new Size(25, 20);
-            label4.TabIndex = 4;
-            label4.Text = "Id:";
+            labelFirstName.Text = "First name:";
             // 
             // labelId
             // 
             labelId.AutoSize = true;
-            labelId.Location = new Point(563, 121);
+            labelId.Location = new Point(563, 54);
             labelId.Name = "labelId";
-            labelId.Size = new Size(50, 20);
+            labelId.Size = new Size(29, 20);
             labelId.TabIndex = 5;
-            labelId.Text = "label1";
+            labelId.Text = "Id: ";
             // 
             // labelEmail
             // 
             labelEmail.AutoSize = true;
             labelEmail.Location = new Point(563, 199);
             labelEmail.Name = "labelEmail";
-            labelEmail.Size = new Size(50, 20);
+            labelEmail.Size = new Size(46, 20);
             labelEmail.TabIndex = 6;
-            labelEmail.Text = "label1";
+            labelEmail.Text = "Email";
             // 
             // labelRole
             // 
             labelRole.AutoSize = true;
             labelRole.Location = new Point(563, 233);
             labelRole.Name = "labelRole";
-            labelRole.Size = new Size(50, 20);
+            labelRole.Size = new Size(42, 20);
             labelRole.TabIndex = 7;
-            labelRole.Text = "label1";
+            labelRole.Text = "Role:";
+            // 
+            // labelLeaveStatus
+            // 
+            labelLeaveStatus.AutoSize = true;
+            labelLeaveStatus.Location = new Point(563, 312);
+            labelLeaveStatus.Name = "labelLeaveStatus";
+            labelLeaveStatus.Size = new Size(92, 20);
+            labelLeaveStatus.TabIndex = 8;
+            labelLeaveStatus.Text = "Leave status:";
+            // 
+            // buttonComplain
+            // 
+            buttonComplain.Location = new Point(536, 346);
+            buttonComplain.Name = "buttonComplain";
+            buttonComplain.Size = new Size(149, 29);
+            buttonComplain.TabIndex = 9;
+            buttonComplain.Text = "Voice grievances";
+            buttonComplain.UseVisualStyleBackColor = true;
+            buttonComplain.Click += buttonComplain_Click;
+            // 
+            // labelAddress
+            // 
+            labelAddress.AutoSize = true;
+            labelAddress.Location = new Point(563, 262);
+            labelAddress.Name = "labelAddress";
+            labelAddress.Size = new Size(65, 20);
+            labelAddress.TabIndex = 10;
+            labelAddress.Text = "Address:";
+            // 
+            // labelLastName
+            // 
+            labelLastName.AutoSize = true;
+            labelLastName.Location = new Point(563, 131);
+            labelLastName.Name = "labelLastName";
+            labelLastName.Size = new Size(79, 20);
+            labelLastName.TabIndex = 11;
+            labelLastName.Text = "Last name:";
+            // 
+            // labelPhone
+            // 
+            labelPhone.AutoSize = true;
+            labelPhone.Location = new Point(563, 165);
+            labelPhone.Name = "labelPhone";
+            labelPhone.Size = new Size(53, 20);
+            labelPhone.TabIndex = 12;
+            labelPhone.Text = "Phone:";
+            // 
+            // labelHourlyRate
+            // 
+            labelHourlyRate.AutoSize = true;
+            labelHourlyRate.Location = new Point(563, 292);
+            labelHourlyRate.Name = "labelHourlyRate";
+            labelHourlyRate.Size = new Size(86, 20);
+            labelHourlyRate.TabIndex = 13;
+            labelHourlyRate.Text = "Hourly rate:";
             // 
             // employeeDashboard
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1254, 597);
+            Controls.Add(labelHourlyRate);
+            Controls.Add(labelPhone);
+            Controls.Add(labelLastName);
+            Controls.Add(labelAddress);
+            Controls.Add(buttonComplain);
+            Controls.Add(labelLeaveStatus);
             Controls.Add(labelRole);
             Controls.Add(labelEmail);
             Controls.Add(labelId);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
             Controls.Add(labelFirstName);
             Name = "employeeDashboard";
             Text = "employeeDashboard";
@@ -133,12 +156,14 @@
         #endregion
 
         private Label labelFirstName;
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
         private Label labelId;
         private Label labelEmail;
         private Label labelRole;
+        private Label labelLeaveStatus;
+        private Button buttonComplain;
+        private Label labelAddress;
+        private Label labelLastName;
+        private Label labelPhone;
+        private Label labelHourlyRate;
     }
 }
--- SWD606_Assignment2/employeeDashboard.cs
+++ SWD606_Assignment2/employeeDashboard.cs
@@ -17,16 +17,24 @@ namespace SWD606_Assignment2
         string email;
         string role;
         int id;
+        string leavestatus;
+        string address;
+        string lastname;
+        string phone;
+        decimal hourlyrate;
+
         SqlConnection connection;
         SqlCommand command;
+
         public employeeDashboard()
         {
             InitializeComponent();
         }
-
+        //employees can use the system to check their leave status,
+        //view salary details, yearly holiday list, and for voicing grievances or resigning.
         private void employeeDashboard_Load(object sender, EventArgs e)
         {
-
+            OpenConnection();
             GetInfo();
         }
         void OpenConnection()
@@ -37,16 +45,45 @@ namespace SWD606_Assignment2
         private void GetInfo()
         {
             firstname = UserSession.Instance.FirstName;
-            labelFirstName.Text = firstname;
+            labelFirstName.Text = "First name: " + firstname;
 
             email = UserSession.Instance.Email;
-            labelEmail.Text = email;
+            labelEmail.Text = "Email: " + email;
 
             role = UserSession.Instance.Role;
-            labelRole.Text = role;
+            labelRole.Text = "Role: " + role;
 
             id = UserSession.Instance.ID;
-            labelId.Text = id.ToString();
+            labelId.Text = "Id: " + id.ToString();
+
+            //leave status not yet implemented
+            //leavestatus = UserSession.Instance.LeaveStatus;
+            //labelLeaveStatus.Text = "Leave status: " + leavestatus;
+
+            string query = "Select Address from Employees where ID = " + id;
+            command = new SqlCommand(query, connection);
+            address = (string)command.ExecuteScalar();
+            labelAddress.Text = "Address: " + address;
+
+            query = "Select LastName from Employees where ID = " + id;
+            command = new SqlCommand (query, connection);
+            lastname = (string)command.ExecuteScalar();
+            labelLastName.Text = "Last name: " + lastname;
+
+            query = "Select Phone from Employees where ID = " + id;
+            command = new SqlCommand(query, connection);
+            phone = (string)command.ExecuteScalar();
+            labelPhone.Text = "Phone: " + phone;
+
+            query = "Select HourlyRate from Employees where ID = " + id;
+            command = new SqlCommand(query, connection);
+            hourlyrate = (decimal)command.ExecuteScalar();
+            labelHourlyRate.Text = "Hourly rate: " + hourlyrate;
+        }
+
+        private void buttonComplain_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
