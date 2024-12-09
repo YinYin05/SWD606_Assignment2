5fffd9182ed8496617184feac27e5da01ee1ae6b
jonat <jonat@JP_F15>
2024-12-10 11:50:36 +1300

    Admin UI Updates

--- SWD606_Assignment2/EmployeesTable.Designer.cs
+++ SWD606_Assignment2/EmployeesTable.Designer.cs
@@ -41,7 +41,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
-            label1.Location = new Point(40, 46);
+            label1.Location = new Point(40, 25);
             label1.Name = "label1";
             label1.Size = new Size(144, 23);
             label1.TabIndex = 0;
@@ -49,18 +49,19 @@
             // 
             // txtSearch
             // 
-            txtSearch.Location = new Point(190, 45);
+            txtSearch.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtSearch.Location = new Point(190, 20);
             txtSearch.Name = "txtSearch";
-            txtSearch.Size = new Size(152, 27);
+            txtSearch.Size = new Size(152, 34);
             txtSearch.TabIndex = 1;
             // 
             // btnSearch
             // 
             btnSearch.BackColor = Color.Yellow;
             btnSearch.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
-            btnSearch.Location = new Point(385, 43);
+            btnSearch.Location = new Point(385, 18);
             btnSearch.Name = "btnSearch";
-            btnSearch.Size = new Size(110, 30);
+            btnSearch.Size = new Size(110, 37);
             btnSearch.TabIndex = 2;
             btnSearch.Text = "Search";
             btnSearch.UseVisualStyleBackColor = false;
@@ -70,11 +71,11 @@
             // 
             dataGridView1.BackgroundColor = SystemColors.Control;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(40, 103);
+            dataGridView1.Location = new Point(40, 72);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(1445, 317);
+            dataGridView1.Size = new Size(1445, 366);
             dataGridView1.TabIndex = 3;
             dataGridView1.CellClick += dataGridView1_CellClick;
             // 
@@ -82,9 +83,9 @@
             // 
             btnReset.BackColor = Color.Cyan;
             btnReset.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
-            btnReset.Location = new Point(521, 43);
+            btnReset.Location = new Point(521, 18);
             btnReset.Name = "btnReset";
-            btnReset.Size = new Size(110, 30);
+            btnReset.Size = new Size(110, 37);
             btnReset.TabIndex = 4;
             btnReset.Text = "Reset";
             btnReset.UseVisualStyleBackColor = false;
@@ -94,7 +95,7 @@
             // 
             btnAddEmployee.BackColor = Color.Fuchsia;
             btnAddEmployee.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
-            btnAddEmployee.Location = new Point(1253, 43);
+            btnAddEmployee.Location = new Point(1251, 20);
             btnAddEmployee.Name = "btnAddEmployee";
             btnAddEmployee.Size = new Size(232, 37);
             btnAddEmployee.TabIndex = 0;
@@ -106,7 +107,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ControlDarkDark;
+            BackColor = Color.Wheat;
             ClientSize = new Size(1515, 450);
             Controls.Add(btnAddEmployee);
             Controls.Add(btnReset);
