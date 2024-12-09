ce7364ad52e1de87d96231de5769ce549ae14ccc
MiikaA1i <764704675@nzse.ac.nz>
2024-12-09 22:05:38 +1300

    trying to pull

--- SWD606_Assignment2/VoiceGrievances.Designer.cs
+++ SWD606_Assignment2/VoiceGrievances.Designer.cs
@@ -28,44 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            textBox1 = new TextBox();
+            SendBTN = new Button();
             label1 = new Label();
+            messageBox = new TextBox();
+            subjectBox = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            emailBox = new TextBox();
             SuspendLayout();
             // 
-            // button1
+            // Send
             // 
-            button1.Location = new Point(316, 169);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "Submit";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(301, 114);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
-            textBox1.TabIndex = 1;
+            SendBTN.Location = new Point(344, 382);
+            SendBTN.Name = "Send";
+            SendBTN.Size = new Size(94, 29);
+            SendBTN.TabIndex = 0;
+            SendBTN.Text = "Send Email";
+            SendBTN.UseVisualStyleBackColor = true;
+            SendBTN.Click += SendBTN_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(188, 121);
+            label1.Location = new Point(147, 205);
             label1.Name = "label1";
-            label1.Size = new Size(74, 20);
+            label1.Size = new Size(99, 20);
             label1.TabIndex = 2;
-            label1.Text = "Grievance";
+            label1.Text = "Message Box:";
+            // 
+            // messageBox
+            // 
+            messageBox.Location = new Point(147, 228);
+            messageBox.Multiline = true;
+            messageBox.Name = "messageBox";
+            messageBox.Size = new Size(496, 113);
+            messageBox.TabIndex = 3;
+            // 
+            // subjectBox
+            // 
+            subjectBox.Location = new Point(147, 154);
+            subjectBox.Name = "subjectBox";
+            subjectBox.Size = new Size(261, 27);
+            subjectBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(147, 131);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Subject:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(147, 47);
+            label3.Name = "label3";
+            label3.Size = new Size(28, 20);
+            label3.TabIndex = 7;
+            label3.Text = "To:";
+            // 
+            // emailBox
+            // 
+            emailBox.Location = new Point(147, 70);
+            emailBox.Name = "emailBox";
+            emailBox.Size = new Size(261, 27);
+            emailBox.TabIndex = 8;
             // 
             // VoiceGrievances
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(emailBox);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(subjectBox);
+            Controls.Add(messageBox);
             Controls.Add(label1);
-            Controls.Add(textBox1);
-            Controls.Add(button1);
+            Controls.Add(SendBTN);
             Name = "VoiceGrievances";
             Text = "VoiceGrievances";
             ResumeLayout(false);
@@ -74,8 +116,13 @@
 
         #endregion
 
-        private Button button1;
-        private TextBox textBox1;
+        private Button SendBTN;
         private Label label1;
+        private TextBox messageBox;
+        private TextBox subjectBox;
+        private Label label2;
+        //private MaskedTextBox managerEmailBox;
+        private Label label3;
+        private TextBox emailBox;
     }
 }
--- SWD606_Assignment2/VoiceGrievances.cs
+++ SWD606_Assignment2/VoiceGrievances.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +18,40 @@ namespace SWD606_Assignment2
         {
             InitializeComponent();
         }
+
+        private void SendBTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // User Inputs
+                string to = emailBox.Text;
+                string subject = subjectBox.Text;
+                string body = messageBox.Text;
+
+                // Outlook Setup
+                string email = "RECAS.Staff@outlook.com";
+                string appPassword = "PasswordTest123@"; // Replace with an app-specific password
+                string host = "smtp-mail.outlook.com";
+                int port = 587;
+
+                using (MailMessage mail = new MailMessage(email, to, subject, body))
+                {
+                    using (SmtpClient smtp = new SmtpClient(host, port))
+                    {
+                        smtp.UseDefaultCredentials = false;
+                        smtp.EnableSsl = true; // Enable secure connection (TLS)
+                        smtp.Credentials = new NetworkCredential(email, appPassword);
+
+                        smtp.Send(mail);
+                        MessageBox.Show("Email Sent!", "Success");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error");
+            }
+        }
+
     }
 }
