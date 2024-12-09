292e3a2fc6511ac17f1745acddd8ce81f4f64a2b
MiikaA1i <764704675@nzse.ac.nz>
2024-12-10 10:31:15 +1300

    updated leave approval UI

--- SWD606_Assignment2/LeaveApprovals.Designer.cs
+++ SWD606_Assignment2/LeaveApprovals.Designer.cs
@@ -44,41 +44,43 @@
             // dgvLeaveApprovals
             // 
             dgvLeaveApprovals.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvLeaveApprovals.Location = new Point(40, 94);
+            dgvLeaveApprovals.Location = new Point(116, 126);
             dgvLeaveApprovals.Name = "dgvLeaveApprovals";
             dgvLeaveApprovals.RowHeadersWidth = 51;
-            dgvLeaveApprovals.Size = new Size(1744, 527);
+            dgvLeaveApprovals.Size = new Size(799, 290);
             dgvLeaveApprovals.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(808, 19);
+            label1.Font = new Font("Nirmala UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(845, 449);
             label1.Name = "label1";
-            label1.Size = new Size(144, 20);
+            label1.Size = new Size(186, 25);
             label1.TabIndex = 1;
             label1.Text = "Leave Approvals List";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(46, 54);
+            label2.Font = new Font("Lucida Fax", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(50, 57);
             label2.Name = "label2";
-            label2.Size = new Size(60, 20);
+            label2.Size = new Size(67, 17);
             label2.TabIndex = 2;
             label2.Text = "Search :";
             // 
             // cmbSearch
             // 
             cmbSearch.FormattingEnabled = true;
-            cmbSearch.Location = new Point(294, 51);
+            cmbSearch.Location = new Point(298, 54);
             cmbSearch.Name = "cmbSearch";
             cmbSearch.Size = new Size(158, 28);
             cmbSearch.TabIndex = 3;
             // 
             // txtSearch
             // 
-            txtSearch.Location = new Point(112, 51);
+            txtSearch.Location = new Point(116, 54);
             txtSearch.Name = "txtSearch";
             txtSearch.Size = new Size(134, 27);
             txtSearch.TabIndex = 4;
@@ -86,15 +88,17 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(255, 54);
+            label3.Font = new Font("Lucida Fax", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(259, 57);
             label3.Name = "label3";
-            label3.Size = new Size(32, 20);
+            label3.Size = new Size(37, 17);
             label3.TabIndex = 5;
             label3.Text = "by :";
             // 
             // btnSearch
             // 
-            btnSearch.Location = new Point(458, 51);
+            btnSearch.Font = new Font("Lucida Fax", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSearch.Location = new Point(462, 54);
             btnSearch.Name = "btnSearch";
             btnSearch.Size = new Size(63, 29);
             btnSearch.TabIndex = 6;
@@ -121,7 +125,8 @@
             // 
             // btnReset
             // 
-            btnReset.Location = new Point(541, 50);
+            btnReset.Font = new Font("Lucida Fax", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnReset.Location = new Point(545, 53);
             btnReset.Name = "btnReset";
             btnReset.Size = new Size(85, 30);
             btnReset.TabIndex = 9;
@@ -133,7 +138,8 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1829, 653);
+            BackColor = Color.White;
+            ClientSize = new Size(1043, 483);
             Controls.Add(btnReset);
             Controls.Add(label4);
             Controls.Add(cmbSortBy);
@@ -144,6 +150,7 @@
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(dgvLeaveApprovals);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "LeaveApprovals";
             Text = "Leave Approvals";
             Load += LeaveApprovals_Load;
