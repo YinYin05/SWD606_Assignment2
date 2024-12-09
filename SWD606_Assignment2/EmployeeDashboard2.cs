09042b3ac4ae9188d2547bce1be59975271010f6
jonat <jonat@JP_F15>
2024-12-10 09:41:47 +1300

    Revert "test"
    This reverts commit 74fa044eabdf309d65e3063ea6b02e0db096aa95.

--- SWD606_Assignment2/EmployeeDashboard2.cs
+++ SWD606_Assignment2/EmployeeDashboard2.cs
@@ -91,7 +91,12 @@ namespace SWD606_Assignment2
         private void OffBoardingButton_Click(object sender, EventArgs e)
         {
             openchildform(new OffBoarding());
-        }      
+        }
+
+        private void AccountInformationButton_Click(object sender, EventArgs e)
+        {
+            openchildform(new AccountInformation());
+        }
 
         private void btnLeaveApprovals_Click(object sender, EventArgs e)
         {
